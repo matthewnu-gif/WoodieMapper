b757de92e01430e7e8f672ffb4d178cd18a5e03b
matthewnu-gif <matthew.n.u@gmail.com>
2025-09-23 16:09:57 +0100

    Add files via upload

--- config.example.js
+++ config.example.js
@@ -1,15 +1,15 @@
 ﻿window.supabaseConfig = {
-  url: '',
-  anonKey: '',
+  url: 'https://ytdultztmwgcuenfqlrp.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl0ZHVsdHp0bXdnY3VlbmZxbHJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg0ODU3NDMsImV4cCI6MjA3NDA2MTc0M30.7JMHAHM0ioGgPNJ2cC7w2BJrZp_xYFAvmAHsJNZKi2c',
   storageBucket: 'boards'
 };
 
 window.firebaseConfig = {
-  apiKey: '',
-  authDomain: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: '',
-  measurementId: ''
+  apiKey: 'AIzaSyAWNyBcxmu_fjVO2XcYa2gBDuybrX6W-bM',
+  authDomain: 'woodiemapper.firebaseapp.com',
+  projectId: 'woodiemapper',
+  storageBucket: 'woodiemapper.firebasestorage.app',
+  messagingSenderId: '168849318295',
+  appId: '1:168849318295:web:bdad6497950be97b5064d2',
+  measurementId: 'G-YT92KPVRC9'
 };
