1f45aa26da1a2a8c1b53d04316efdb3ac8d8390b
MattU <matthew.n.u@gmail.com>
2026-02-05 15:49:46 +0000

    Add files via upload

--- config.example.js
+++ config.example.js
@@ -1,6 +1,6 @@
 ﻿window.supabaseConfig = {
-  url: 'https://ytdultztmwgcuenfqlrp.supabase.co',
-  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl0ZHVsdHp0bXdnY3VlbmZxbHJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg0ODU3NDMsImV4cCI6MjA3NDA2MTc0M30.7JMHAHM0ioGgPNJ2cC7w2BJrZp_xYFAvmAHsJNZKi2c',
+  url: 'https://yjpdfrtldnxosohzfxdp.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlqcGRmcnRsZG54b3NvaHpmeGRwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk3ODg3OTcsImV4cCI6MjA4NTM2NDc5N30.kAyK4iRIWwiwo-A5s5tPKRwKFgQd4EhgZgrl4ZacfHw',
   storageBucket: 'boards'
 };
 
